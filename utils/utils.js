1e6248ca7dabbc6a5f1db6937faaa31f2335702d
lovelyoyrmia <mokalulovelyo@gmail.com>
2022-08-20 13:06:12 +0700

    define role object

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,20 @@
-export const ROLE = {
-  admin: "ADMIN",
-  user: "USER",
+const ROLE = {
+  user: 2001,
+  admin: 1294,
+  patient: 3012,
+  doctor: 4011,
 };
+
+const DATA = {
+  booking: "BOOKING",
+  patient: "PATIENT",
+  doctor: "DOCTOR",
+  admin: "ADMIN",
+};
+
+const DOCTOR = {
+  umum: "UMUM",
+  gigi: "GIGI",
+};
+
+module.exports = { ROLE, DATA, DOCTOR };
